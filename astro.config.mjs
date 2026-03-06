4d038c645a7b92244edc19d8ea301aa482b0d215
Dennis Hartrampf <d.hartrampf@bvv.de>
2026-03-06 23:54:48 +0100

    Migrate vanilla website to Astro framework

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://neurodiv.dev',
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'de'],
+    routing: {
+      prefixDefaultLocale: true
+    }
+  },
+
+  integrations: [sitemap()]
+});
