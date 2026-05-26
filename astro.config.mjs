d88f1cd09a51b2895e2833997e8e46a48b64da72
Dennis Hartrampf <d.hartrampf@bvv.de>
2026-05-26 22:17:55 +0200

    Rebranding: Calm Color Reveal

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,5 +17,18 @@ export default defineConfig({
     }
   },
 
+  redirects: {
+    '/asmr-color-reveal-kids': '/calm-color-reveal',
+    '/en/asmr-color-reveal-kids': '/en/calm-color-reveal',
+    '/en/asmr-color-reveal-kids/privacy-policy': '/en/calm-color-reveal/privacy-policy',
+    '/de/asmr-color-reveal-kids': '/de/calm-color-reveal',
+    '/de/asmr-color-reveal-kids/datenschutz': '/de/calm-color-reveal/datenschutz',
+    '/en/asmr-color-reveal-kids/preregistration': '/en/calm-color-reveal/preregistration',
+    '/de/asmr-color-reveal-kids/preregistration': '/de/calm-color-reveal/preregistration',
+    '/en/asmr-color-reveal-kids/prereg-terms': '/en/calm-color-reveal/prereg-terms',
+    '/de/asmr-color-reveal-kids/prereg-terms': '/de/calm-color-reveal/prereg-terms',
+    '/asmr-color-reveal-kids/privacy-policy': '/calm-color-reveal/privacy-policy'
+  },
+
   integrations: [sitemap()]
 });
